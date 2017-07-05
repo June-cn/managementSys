f48823600a41e733a49d214a7c107f0910fc45e8
June-xa <june.yu@yamibuy.com>
2017-07-05 10:03:00 +0800

    添加登陆页面

--- app/js/login.js
+++ app/js/login.js
@@ -1,3 +1,30 @@
 /**
  * Created by june.yu on 2017/7/4.
  */
+(function (window) {
+    var vm = this;
+    vm.login = {
+        init: function () {
+            this.login();
+        },
+        login: function () {
+            $('.login-button').on('click',function () {
+                var formData = {};
+                formData.username = $('#user-name').val();
+                formData.password = $('#user-psw').val();
+                $.ajax({
+                    type: type,
+                    url: url,
+                    // contentType: "application/json",
+                    // headers: {"token": token},
+                    dataType: "json",
+                    // data: JSON.stringify(formData),
+                    success: function (data) {
+                        console.log(data);
+                    },
+                });
+            });
+        }
+    };
+    vm.login.init();
+}(window));
